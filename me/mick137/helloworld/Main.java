1febd66ea7c7c22932816e791ed7250457de5daf
[O] iMPDevMC <47728875+iDevMC@users.noreply.github.com>
2019-10-19 21:45:04 -0400

    Laugh and you're dead.

---
+++ me/mick137/helloworld/Main.java
@@ -0,0 +1,13 @@
+# Note: i made this when i was 9. So chill budy.
+
+package me.mick137.helloworld;
+
+public class Main {
+	
+	public static void mick137(String[] args) {
+		
+		System.out.print("Hello World!!!!!");
+		
+	}
+
+}
